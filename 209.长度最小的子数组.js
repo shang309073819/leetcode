8ac469e05226566ce969190a756b8519bb719325
弓远 <shangshang.css@alibaba-inc.com>
2020-01-26 22:54:21 +0800

    UPDATE

--- 209.长度最小的子数组.js
+++ 209.长度最小的子数组.js
@@ -10,7 +10,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var minSubArrayLen = function(s, nums) {
-    
+var minSubArrayLen = function (s, nums) {
+    let i = 0, j = 0, sum = nums[0], num = 0;
+    while (i < nums.length && j < nums.length) {
+        if (sum < s) {
+            j++;
+            sum = sum + nums[j];
+        } else {
+            num = num > 0 ? Math.min(num, j - i + 1) : j - i + 1;
+            sum = sum - nums[i];
+            i++;
+        }
+    }
+    return num;
 };
 // @lc code=end
+
+
+/**
+ * 滑动窗口, 定义左右指针
+ */
